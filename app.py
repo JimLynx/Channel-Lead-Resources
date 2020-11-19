35fd342d73d34033d64b79921a73bcc12f57936a
JimLynx <jim.lynx@gmail.com>
2020-11-19 23:13:18 +0000

    Test Heroku deployment

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from os import path
-if os.path.exists('env.py'):
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
-app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
