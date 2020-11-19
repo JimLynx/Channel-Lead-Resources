17312a80a9f770e93769c13acb8cd633161e21de
JimLynx <jim.lynx@gmail.com>
2020-11-19 22:24:44 +0000

    Create initial route and test data

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 import os
-from flask import Flask
-if os.path.exists("env.py"):
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if os.path.exists('env.py'):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.secret_key = os.environ.get('SECRET_KEY')
 
-@app.route("/")
-def hello():
-    return "Testing app"
+mongo = PyMongo(app)
 
 
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
+@app.route('/')
+@app.route('/resources')
+def resources():
+    resources = mongo.db.cl_resources.find()
+    return render_template('resources.html', resources=resources)
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
             debug=True)
