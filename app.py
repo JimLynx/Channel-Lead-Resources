68edfb47fa66d1eaa64ccb01e47e3c524e954f67
JimLynx <jim.lynx@gmail.com>
2020-11-20 20:05:27 +0000

    Add route for Home page

--- app.py
+++ app.py
@@ -19,6 +19,11 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
 @app.route('/resources')
 def resources():
     resources = list(mongo.db.cl_resources.find())
