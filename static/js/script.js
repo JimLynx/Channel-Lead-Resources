de484c808372f9142b6684ee3719e9674ea52f58
JimLynx <jim.lynx@gmail.com>
2020-11-26 10:55:55 +0000

    Create function to remove Flash messages after period

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+
+    // clear flash messages after 5 seconds
+    $(".alert").delay(5000).slideUp(1000);
+
+    // set date format
     $("#date").datepicker({
         format: 'dd/mm/yyyy',
         startDate: '-3d'
