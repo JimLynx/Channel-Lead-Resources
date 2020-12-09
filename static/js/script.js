29ee0490a71cc9a1339719868b26f781edddae27
JimLynx <jim.lynx@gmail.com>
2020-12-09 14:23:11 +0000

    Create function to stop videos playing after cancel button clicked

--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
     // clear flash messages after 5 seconds
     $(".alert").delay(5000).slideUp(1000);
 
+    // stop videos playing when cancel button on modal clicked
+    $('.cancelVid').click(function () {
+        $('iframe').attr('src', $('iframe').attr('src'));
+    });
 });
