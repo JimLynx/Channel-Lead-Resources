fdd2cb2e514991489eeb5dc2f2046e7772c8b665
JimLynx <jim.lynx@gmail.com>
2020-12-12 17:04:59 +0000

    Add function to collapse resources as next is selected

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,27 @@
 $(document).ready(function () {
 
-    // clear flash messages after 5 seconds
-    $(".alert").delay(5000).slideUp(1000);
+  // clear flash messages after 5 seconds
+  $(".alert").delay(5000).slideUp(1000);
 
-    // stop videos playing when cancel button on modal clicked
-    $('.cancelVid').click(function () {
-        $('iframe').attr('src', $('iframe').attr('src'));
-    });
+  // stop videos playing when cancel button on modal clicked
+  $('.cancelVid').click(function () {
+    $('iframe').attr('src', $('iframe').attr('src'));
+  });
 
-    // prevent whitespace for username and passwords inputs
-    // Credit: adapted from https://stackoverflow.com/a/14236954
-    $("input#username, input#password").on({
-        keydown: function(e) {
-          if (e.which === 32)
-            return false;
-        },
-        change: function() {
-          this.value = this.value.replace(/\s/g, "");
-        }
-      });
+  // prevent whitespace for username and passwords inputs
+  // Credit: adapted from https://stackoverflow.com/a/14236954
+  $("input#username, input#password").on({
+    keydown: function (e) {
+      if (e.which === 32)
+        return false;
+    },
+    change: function () {
+      this.value = this.value.replace(/\s/g, "");
+    }
+  });
+
+  // collapse resources if another clicked
+  $('.resource-btn').click(function () {
+    $('.collapse').collapse('hide');
+  });
 });
